53f54537833afda37cbf1f387d88672106478129
Dima <logosmk@gmail.com>
2018-01-13 01:11:26 +0300

    init

---
+++ test/JavaScript.js
@@ -0,0 +1,6 @@
+var SureName = "Dima";
+var Surefamaly = "Chesnokov";
+var years = 51;
+var DateOfBirth = 1965;
+var Heppy=0.75;
+document.write("Variable =" + SureName + "<br />" + Surefamaly +"<br />" + years + "<br />" + DateOfBirth + "<br />" + Heppy);
